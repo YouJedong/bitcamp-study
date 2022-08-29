72adf3e30cbfd4f721c2b1e4ea0a040b073d0c43
YouJedong <mraiou1@naver.com>
2022-08-29 13:36:30 +0900

    주말 공부

--- board-appStudy/app-clientStudy/src/main/java/com/bitcamp/study/myTest.java
+++ board-appStudy/app-clientStudy/src/main/java/com/bitcamp/study/myTest.java
@@ -3,18 +3,21 @@ package com.bitcamp.study;
 import java.util.Scanner;
 
 public class myTest {
-
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    int[] chess = {1, 1, 2, 2, 2, 8};
-    int[] inChess = new int[6];
+    int H = sc.nextInt();
+    int M = sc.nextInt();
 
-    for(int i = 0; i < inChess.length; i++) {
-      inChess[i] = sc.nextInt();
-      System.out.print(chess[i] - inChess[i] + " ");
+    if ((M - 45) < 0) { 
+      if (H == 0) {
+        H = 24;
+      }
+      System.out.print(--H);    
+      System.out.print(M + 15); 
+    } else {
+      System.out.print(H);
+      System.out.print(M - 45);
     }
   }
 }
-
-
