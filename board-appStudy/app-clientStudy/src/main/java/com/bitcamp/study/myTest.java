35a0f9d8a4303968456d1139de32602f89b84f4c
YouJedong <mraiou1@naver.com>
2022-08-30 17:00:41 +0900

    8/30 백준 문제 연습

--- board-appStudy/app-clientStudy/src/main/java/com/bitcamp/study/myTest.java
+++ board-appStudy/app-clientStudy/src/main/java/com/bitcamp/study/myTest.java
@@ -2,22 +2,23 @@ package com.bitcamp.study;
 
 import java.util.Scanner;
 
-public class myTest {
+class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
     int H = sc.nextInt();
     int M = sc.nextInt();
+    int needTime = sc.nextInt();
+    int TH = (needTime / 10) / 6;  
 
-    if ((M - 45) < 0) { 
-      if (H == 0) {
-        H = 24;
+    if ((M + needTime) > 60) {
+      if (H + TH < 23) {
+        H = -1;
       }
-      System.out.print(--H);    
-      System.out.print(M + 15); 
+      System.out.print((H + TH) + " " + ((60 * TH) - (M + needTime))); 
     } else {
-      System.out.print(H);
-      System.out.print(M - 45);
+      System.out.print(H++ + " " + (M + needTime));
     }
   }
+
 }
