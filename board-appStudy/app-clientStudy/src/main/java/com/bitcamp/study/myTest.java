3d023f5e47402db1010db320bf0da96baa666343
YouJedong <mraiou1@naver.com>
2022-09-01 20:31:40 +0900

    9/1 복습

--- board-appStudy/app-clientStudy/src/main/java/com/bitcamp/study/myTest.java
+++ board-appStudy/app-clientStudy/src/main/java/com/bitcamp/study/myTest.java
@@ -9,16 +9,15 @@ class Main {
     int H = sc.nextInt();
     int M = sc.nextInt();
     int needTime = sc.nextInt();
-    int TH = (needTime / 10) / 6;  
+    int TH = (needTime / 10) / 6;
 
     if ((M + needTime) > 60) {
-      if (H + TH < 23) {
-        H = -1;
+      if (H + TH > 23) {
+        System.out.print(((H + TH) - 24) + " " + ((60 * TH) - (M + needTime)));
       }
       System.out.print((H + TH) + " " + ((60 * TH) - (M + needTime))); 
     } else {
-      System.out.print(H++ + " " + (M + needTime));
+      System.out.print(H + TH + " " + (M + needTime));
     }
   }
-
 }
