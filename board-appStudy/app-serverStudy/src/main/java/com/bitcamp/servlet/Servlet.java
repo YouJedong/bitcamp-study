6a27ab67b23a970b588cb453064a9b4a4e8a7e49
YouJedong <mraiou1@naver.com>
2022-09-21 22:36:50 +0900

    9/21 복습

--- board-appStudy/app-serverStudy/src/main/java/com/bitcamp/servlet/Servlet.java
+++
@@ -1,9 +0,0 @@
-package com.bitcamp.servlet;
-
-import java.io.PrintWriter;
-import java.util.Map;
-
-public interface Servlet {
-
-  void service(Map<String,String> paramMap, PrintWriter out) throws Exception;
-}
